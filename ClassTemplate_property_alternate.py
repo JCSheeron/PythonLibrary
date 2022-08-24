e6c4ec749e1e8888e8a6eeb57fb7e697db33e873
Jeff Sheeron <jcs@san.rr.com>
2022-08-24 13:49:27 -0700

    Updated ClassTemplate and included one with an alternate coding of a property.

--- ClassTemplate_property_alternate.py
+++ ClassTemplate_property_alternate.py
@@ -8,15 +8,25 @@
 #
 
 class ClassName(object):
+    """Single line summary description right under ClassName
+
+    A more complete description after a blank line.
+    Blah Blah Blah
+    Blah Blah Blah
+    Follow this with the closing of the quotes and a blank line.
+    Last line of document."""
+
     # class constants
-    # (none)
+    CONST_NAME = 666
 
     # default ctor
     def __init__(self):
+        """ Document ctor"""
         pass
 
     # ctor with params
     def __init__(self, paramA, paramB):
+        """ Document ctor"""
         # _ prefix is a convention for private or something that should not
         # accessed outside the class. But the _name isn't enforced as private.
         # it is just considered a bad style violation to access it externally.
@@ -25,44 +35,49 @@ class ClassName(object):
         pass
 
     def __repr__(self):
+        """ Document fct"""
+
         # __repr__ should create a 'representation that
         # should look like a valid Python Expression that could be used
         # to recreate an object with the same value.'
-        # The goal of __repr__ is to be unambiguous.
+        # The goal of __repr__ is to be unambiguous. This could be returning
+        # a dictionary or list for example that captures the state of the
+        # class.
         # Implement __repr__ for any class you implement.
         pass
 
     def __str__(self):
+        """ Document fct"""
+
         # The goal of __str__ is to create a string representation
         # of the object that is readable to a user (not a programmer).
         # Implement __str__ if you think it would be useful to have a string
-        # version which errs on the side of readability in favor of more
-        # ambiguity
+        # version which errs on the side of readability perhaps at the expense
+        # of ambiguity
         pass
 
-    def MyMethod1(self):
-        pass
+    def __double_method(self): # for mangling
+        """ Document fct"""
 
-    def MyMethod1(self, ParamA):
-        pass
-
-    def __nameMangled_method(self): # for mangling
         # double underscore names will mangle the name with the class name
         # to avoid conflicts of attribute names between classes
         pass
 
-    # properties
-    @property
-    def ParamA(self):
+    # Getters and Setters and Deleters for ParamA
+    # note the underscore to treat them as private.
+    def _get_paramA(self):
         return self._paramA
 
-    @ParamA.setter
-    def ParamA(self, newValue):
-        self._paramA = newValue
-        pass
+    def _set_paramA(self, newValue):
+        self._paramA = value
 
-    @ParamA.deleter
-    def ParamA(self):
-        # note deleters may not always be needed
+    def _del_radius(self):
         del self._paramA
-        pass
+
+    paramA = property(
+            fget=_get_paramA,
+            fset=_set_paramA,
+            fdel=_del_radius,
+            doc="The ParamA property."
+            )
+
